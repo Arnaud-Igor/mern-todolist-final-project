27bf9fff42582ad98dee98b50026f7274fdf7850
MacBook Pro <macbookpro@192.168.1.4>
2024-08-15 14:27:28 +0000

    first commit

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -8,12 +8,20 @@ export const verifyToken = (req, res, next) => {
     return next(errorHandler(401, "Unauthorized"));
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      return next(errorHandler(403, "Forbidden"));
-    }
-
-    req.user = user;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
     next();
-  });
+  } catch (error) {
+    return res.status(401).json({ message: error.message });
+  }
+
+  // jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  //   if (err) {
+  //     return next(errorHandler(403, "Forbidden"));
+  //   }
+
+  //   req.user = user;
+  //   next();
+  // });
 };
