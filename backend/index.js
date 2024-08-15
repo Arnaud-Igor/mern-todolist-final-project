dc7a29da8e3d344b1c591d7f2885a4db9900808e
MacBook Pro <macbookpro@192.168.1.4>
2024-08-15 14:01:53 +0000

    first commit

--- backend/index.js
+++ backend/index.js
@@ -22,12 +22,14 @@ const app = express();
 // to make input as json
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: ["https://mern-todolist-app-ui.vercel.app"],
-    credentials: true,
-  })
-);
+app.use(cors({ credentials: true }));
+
+// app.use(
+//   cors({
+//     origin: ["https://mern-todolist-app-ui.vercel.app"],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send("Hello World");
